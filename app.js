039897abb2ef236774387c7e0eb6dd594a91ec05
AKTIV <Azizbek>
2023-03-02 18:10:29 +0500

    hello

--- app.js
+++ app.js
@@ -1,83 +1,23 @@
-// // // 1. Function Declaration == Funksiya e'lon qilish
-// // // function foo(t) {
-// // //     return t;
-// // // }
-
-// // // foo(1111)
-
-// // // //2. Function exprission
-
-// // // const abs = function (uuu) {
-// // //     console.log(uuu);
-// // // };
-// // // abs(555)
-
-// // // //3. Arrow Function
-
-// // // const aaa = (a) => console.log(a);
-
-// // // aaa("arrow");
-
-// // let time = prompt(`time`)
-// // let fname = prompt(`fname`)
-
-// // const text = function (tttt) {
-// //     if (tttt > 8 && tttt <= 12) {
-// //         alert(`Good morning`)
-// //     } else if (tttt > 13 && tttt <= 20) {
-// //         alert(`Good afternoon`)
-// //     } else {
-// //         alert(`Good night`)
-// //     }
-// // }
-
-// // alert(text(time))
-
-// // const greeting = (f, t) => t > 8 && t < 18 ? alert(`Good afternoon, ${f}`) : alert(`Good night, ${f}`);
-
-// // greeting(fname, time);
-
-
-// let text = prompt("5+5=")
-// let tttt = prompt(`5 / 5=`)
-
-// function foo(text) {
-//     if (text == 10) {
-//         alert(`Siz togri javob berdingiz 5 + 5 = 10`)
-//     } else if (text == 0) {
-//         alert(`Siz notogri javob berdingiz 5 + 5 = 10`)
-//     } else if (tttt == 1) {
-//         alert(`siz togri javob berdingiz 5 / 5 = 1`)
-//     } else {
-//         alert(`siz notogri javob berdingiz 5 / 5 = 1`)
-//     }
-// }
-// alert(foo(tttt))
-// alert(foo(text))
-
-const week = 1
+const week = prompt(`Auto`)
 
 switch (week) {
-    case 1:
-        console.log(`Monday`);
+    case `nexia R3`:
+        console.log(`150 000 000`);
         break;
-    case 2:
-        console.log(`Tuesday`);
+    case `cobalt`:
+        console.log(`170 000 000`);
         break;
-    case 3:
-        console.log(`Wednesday`);
+    case `gentra`:
+        console.log(`200 000 000`);
         break;
-    case 4:
-        console.log(`Thursday`);
+    case `malibu`:
+        console.log(`350 000 000`);
         break;
-    case 5:
-        console.log(`Friday`);
+    case `captiva`:
+        console.log(`300 000 000`);
         break;
-    case 6:
-        console.log(`Saturday`);
-        break;
-    case 7:
-        console.log(`Sonday`);
+    case `traiblazer`:
+        console.log(`400 000 000`);
         break;
     default:
         console.log(`Enter the number of week days`);
